c17299d2dfc8f2e4b793079e9f85a3cc933be8a9
leog <leonardo.gomez@viajobien.com>
2017-12-01 17:27:06 -0300

    fix1

--- src/Main.js
+++ src/Main.js
@@ -15,18 +15,18 @@ const Main = () => (
         <Router>
             <div>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/home">Home</Link></li>
                     <li><Link to="/huracan">Huracan</Link></li>
                     <li><Link to="/losrodriguez">Los Rodriguez</Link></li>
-                    <li><Link to="/mobs">MobS</Link></li>
+                    {/*      <li><Link to="/mobs">MobS</Link></li>*/}
                 </ul>
 
                 <hr />
 
-                <Route exact path="/" component={Home} />
+                <Route exact path="/home" component={Home} />
                 <Route path="/huracan" component={Huracan} />
                 <Route path="/losrodriguez" component={LosRodriguez} />
-                <Route path="/mobs" component={MobS} />
+                {/*<Route path="/mobs" component={MobS} />*/}
             </div>
         </Router>
 
