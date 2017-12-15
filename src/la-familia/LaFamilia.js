55116af973862d15aeb986587299f2c2c79eaab2
leog <leonardo.gomez@viajobien.com>
2017-12-15 15:21:14 -0300

    added la familia phone number

--- src/la-familia/LaFamilia.js
+++ src/la-familia/LaFamilia.js
@@ -8,6 +8,7 @@ class LaFamilia extends Component {
             <div className="LaFamilia">
                 <header>
                     <h1>La Familia</h1>
+                    <h2>Tel: 4911-4692 / 11-4024-6544 </h2>
                 </header>
                 <br />
                 <img src={lafamilia} alt="la familia"
