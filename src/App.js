43d39f9a7f2633314724d81ee9165f93843623f8
leog <leonardo.gomez@viajobien.com>
2017-12-22 16:22:04 -0300

    new layout

--- src/App.js
+++ src/App.js
@@ -41,10 +41,10 @@ class App extends Component {
               <li key={order.id} className="list-group-item">
                 <span className="list-item">{order.text}</span>
                 <span
-                  className="list-item delete-button pull-right"
+                  className="list-item  pull-right glyphicon glyphicon-remove"
+                  aria-hidden="true"
                   onClick={() => this.deleteOrder(order.id)}
                 >
-                  &#x2715;
                 </span>
               </li>
             )
@@ -68,25 +68,13 @@ class App extends Component {
           <div className="row">
             <div className="col-md-6">
               <div className="form-inline">
-                <div className="form-group">
-                  <input
-                    className="form-control"
-                    placeholder="milaCompleta"
-                    onChange={event => this.setState({ text: event.target.value })}
-                  />
-                </div>
+
                 <br></br>
               </div>
             </div>
             <div className="col-md-6">
               <Opciones />
-              <button type="button"
-                className="btn btn-success btn-block"
-                onClick={() => this.addOrder()}
-              >
-                Add Order
-            </button>
-              <br></br>
+
             </div>
           </div>
 
@@ -94,13 +82,35 @@ class App extends Component {
             <div className="col-md-3">
             </div>
             <div className="col-md-6">
-              {this.renderOrders()}
+
             </div>
             <div className="col-md-3">
             </div>
           </div>
-
-          <Main />
+          <div className="row">
+            <div className="col-md-2">
+              <div className="form-group">
+                <input
+                  className="form-control"
+                  placeholder="milaCompleta"
+                  onChange={event => this.setState({ text: event.target.value })}
+                />
+              </div>
+              <button type="button"
+                className="btn btn-success btn-block"
+                onClick={() => this.addOrder()}
+              >
+              <span className="glyphicon glyphicon-plus pull-left"> </span>
+                Add Order 
+            </button>
+              <br></br>
+              <hr></hr>
+              {this.renderOrders()}
+            </div>
+            <div className="col-md-10">
+              <Main />
+            </div>
+          </div>
         </div>
       </div>
     );
