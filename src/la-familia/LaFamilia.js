3e7c285fff23e6e1982ef73a86d40452b1148a2c
leog <leonardo.gomez@viajobien.com>
2018-05-21 16:19:38 -0300

    images optimized :hushed:

--- src/la-familia/LaFamilia.js
+++ src/la-familia/LaFamilia.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import '../App.css';
 import lafamilia from './la-familia.jpg';
+import lafamiliaa2 from './la-familia-a2.jpg';
+import lafamiliab2 from './la-familia-b2.jpg';
 
 class LaFamilia extends Component {
   render() {
@@ -11,13 +13,30 @@ class LaFamilia extends Component {
           <h2>Tel: 4911-4692 / 11-4024-6544 </h2>
         </header>
         <br />
+
+        <img
+          src={lafamiliaa2}
+          alt="la familia"
+          title="la familia"
+          className="img-responsive img-rounded center-block"
+        />
+        <br />
+        <img
+          src={lafamiliab2}
+          alt="la familia"
+          title="la familia"
+          className="img-responsive img-rounded center-block"
+        />
+
+        <br />
+        <hr />
+        <br />
         <img
           src={lafamilia}
           alt="la familia"
           title="la familia"
           className="img-responsive img-rounded center-block"
         />
-        <br />
       </div>
     );
   }
